7dfe8d25088139cf32e404d59040394b38e97cfd
Peterburg52 <kavasakisig@gmail.com>
2025-04-04 16:33:37 +0300

    Update rate_limiter.py

--- bot/middlewares/rate_limiter.py
+++ bot/middlewares/rate_limiter.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from bot.services.limiter import limiter
 
+class RateLimiterMiddleware(BaseMiddleware):
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        if not await limiter.acquire():
+            await message.answer("Вы слишком часто отправляете запросы. Попробуйте позже.")
+            raise CancelHandler()
