37c76e250b6a3153369648ec560b5f82aa52998e
Peterburg52 <kavasakisig@gmail.com>
2025-04-04 18:43:01 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,29 +2,28 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
-import asyncio
 from bot.config.settings import TOKEN, WEBHOOK_URL
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+# Импортируем router ПОСЛЕ создания Dispatcher
+from bot.handlers.menu_handler import router as menu_router
+
+dp.include_router(menu_router)
 
 async def set_commands():
     await bot.set_my_commands([
         BotCommand(command="start", description="Запустить бота"),
     ])
 
-async def main():
-    from bot.handlers.menu_handler import router as menu_router  # Переместили импорт внутрь функции
-    dp.include_router(menu_router)
-
+async def on_startup():
     await bot.set_webhook(WEBHOOK_URL)
     await set_commands()
 
-    app = web.Application()
-    setup_application(app, dp, bot)
-    app.router.add_post("/webhook", SimpleRequestHandler(dp, bot).handle)
-
-    return app
+app = web.Application()
+setup_application(app, dp, bot)
+app.router.add_post("/webhook", SimpleRequestHandler(dp, bot).handle)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    web.run_app(app, port=8000)
