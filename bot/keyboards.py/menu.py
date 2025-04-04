a9b9c6e68a2041cca053fc166e146c131f869133
Peterburg52 <kavasakisig@gmail.com>
2025-04-04 16:32:05 +0300

    Update menu.py

--- bot/keyboards.py/menu.py
+++ bot/keyboards.py/menu.py
@@ -1,1 +1,8 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def create_menu_keyboard(menu_level):
+    keyboard = InlineKeyboardMarkup()
+    for name in menu_level:
+        if menu_level[name] is not None:
+            keyboard.add(InlineKeyboardButton(name, callback_data=name))
+    return keyboard
