22f7f19edddb660eefc9a5cdaaf120655253268d
Peterburg52 <kavasakisig@gmail.com>
2025-04-04 16:31:29 +0300

    Update menu_config.py

--- bot/config/menu_config.py
+++ bot/config/menu_config.py
@@ -1,1 +1,15 @@
-
+MENU_STRUCTURE = {
+    "Главное меню": {
+        "📷 Фото": {
+            "🌆 Пейзажи": {
+                "image": "https://example.com/landscape.jpg",
+                "description": "Красивый пейзаж."
+            },
+            "🏙 Город": {
+                "image": "https://example.com/city.jpg",
+                "description": "Ночной город."
+            }
+        },
+        "🔙 Назад": None
+    }
+}
