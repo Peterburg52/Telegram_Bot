aba30dd172b69ca6040c4c5a4f24120b46eeeee6
Peterburg52 <kavasakisig@gmail.com>
2025-04-04 16:30:09 +0300

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
